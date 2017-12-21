aa34230d5fc247a85469fe8c9e3f3fd03419ab51
purnolu15 <purnolu15@DST20712>
2017-12-21 11:42:01 +0100

    Solved, filling random enemy generation

--- EpicQuest_0.1.0/EpicQuest_0.1.0/Classes/Enemy_Generator.cs
+++ EpicQuest_0.1.0/EpicQuest_0.1.0/Classes/Enemy_Generator.cs
@@ -15,37 +15,178 @@ using System.Windows.Shapes;
 
 namespace EpicQuest_0._1._0.Classes
 {
-    public class Enemy_Generator : Pages.Dungeon
+    public class Enemy_Generator
     {
-        public void EnemyGen(int cavetype, Image Position1, Image Position2, Label Position1HP, Label Position2HP) // 1-Dungeon, 2-Forest, 3-Catacombs, 4-Depths, 5-Cave, 6-All
+        public void EnemyGen(int cavetype, Image Position1, Image Position2, Label Position1HP, Label Position2HP, Button Enemy1, Button Enemy2) // 1-Dungeon, 2-Forest, 3-Catacombs, 4-Depths, 5-Cave, 6-All
         {
 
-            //EnemyType.Next(1, 6)
-
             Random EnemyType = new Random();
-            int enemytype1 = 1;
-            int enemytype2 = 1;
+            int enemytype1 = EnemyType.Next(1, 6);
+            int enemytype2 = EnemyType.Next(1, 6);
 
             if (cavetype == 1)
             {
                 if (enemytype1 == 1)
                 {
                     Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Behemoth.png"));
-                    //Position?
 
                     Position1HP.Content = "90";
+                    Enemy1.Content = "Behemoth";
                 }
 
                 if (enemytype2 == 1)
                 {
                     Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Behemoth.png"));
-                    //Position?
 
                     Position2HP.Content = "90";
+                    Enemy2.Content = "Behemoth";
+                }
+
+                if (enemytype1 == 2)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Magic_Dragon.png"));
+
+                    Position1HP.Content = "65";
+                    Enemy1.Content = "Magic Dragon";
+                }
+
+                if (enemytype2 == 2)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Magic_Dragon.png"));
+
+                    Position2HP.Content = "65";
+                    Enemy2.Content = "Magic Dragon";
+                }
+                if (enemytype1 == 3)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Brachioraidos.png"));
+
+                    Position1HP.Content = "95";
+                    Enemy1.Content = "Brachioraidos";
+                }
+
+                if (enemytype2 == 3)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Brachioraidos.png"));
+
+                    Position2HP.Content = "95";
+                    Enemy2.Content = "Brachioraidos";
+                }
+
+                if (enemytype1 == 4)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Red_Dragon.png"));
+
+                    Position1HP.Content = "80";
+                    Enemy1.Content = "Red Dragon";
+                }
+
+                if (enemytype2 == 4)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Red_Dragon.png"));
+
+                    Position2HP.Content = "80";
+                    Enemy2.Content = "Red Dragon";
+                }
+
+                if (enemytype1 == 5)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Trap_Door.png"));
+
+                    Position1HP.Content = "100";
+                    Enemy1.Content = "Trap Door";
+                }
+
+                if (enemytype2 == 5)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Trap_Door.png"));
+
+                    Position2HP.Content = "100";
+                    Enemy2.Content = "Trap Door";
                 }
             }
 
+            if (cavetype == 2)
+            {
+                if (enemytype1 == 1)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Hundlegs.png"));
 
+                    Position1HP.Content = "75";
+                    Enemy1.Content = "Hundlegs";
+                }
+
+                if (enemytype2 == 1)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Hundlegs.png"));
+
+                    Position2HP.Content = "75";
+                    Enemy2.Content = "Hundlegs";
+                }
+
+                if (enemytype1 == 2)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Gigantoad.png"));
+
+                    Position1HP.Content = "65";
+                    Enemy1.Content = "Gigantoad";
+                }
+
+                if (enemytype2 == 2)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Gigantoad.png"));
+
+                    Position2HP.Content = "65";
+                    Enemy2.Content = "Gigantoad";
+                }
+                if (enemytype1 == 3)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Death_Flower.png"));
+
+                    Position1HP.Content = "75";
+                    Enemy1.Content = "Death_Flower";
+                }
+
+                if (enemytype2 == 3)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Death_Flower.png"));
+
+                    Position2HP.Content = "75";
+                    Enemy2.Content = "Death_Flower";
+                }
+
+                if (enemytype1 == 4)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Blood_Flower.png"));
+
+                    Position1HP.Content = "80";
+                    Enemy1.Content = "Blood_Flower";
+                }
+
+                if (enemytype2 == 4)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Blood_Flower.png"));
+
+                    Position2HP.Content = "80";
+                    Enemy2.Content = "Blood_Flower";
+                }
+
+                if (enemytype1 == 5)
+                {
+                    Position1.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Flying_Eyes.png"));
+
+                    Position1HP.Content = "60";
+                    Enemy1.Content = "Flying_Eyes";
+                }
+
+                if (enemytype2 == 5)
+                {
+                    Position2.Source = new BitmapImage(new Uri("pack://application:,,,/Enemies/Flying_Eyes.png"));
+
+                    Position2HP.Content = "60";
+                    Enemy2.Content = "Flying_Eyes";
+                }
+            }
 
         }
     }
